ef064d922b77307bbd56db698f387e25d662cabc
milanzagorac <zagoracm@gmail.com>
2021-05-26 20:53:40 -0400

    working on the front end conde

--- static/app.js
+++ static/app.js
@@ -1,60 +1,65 @@
-let getButton = document.getElementById('get-btn')
-let addButton = document.getElementById('add-btn')
-let flavorInput = document.querySelector('#flavor')
-let ratingInput = document.querySelector('#rating')
-let sizeInput = document.querySelector('#size')
-let imageInput = document.querySelector('#image')
+let getButton = document.getElementById('get-btn');
+let addButton = document.getElementById('add-btn');
+let flavorInput = document.querySelector('#flavor');
+let ratingInput = document.querySelector('#rating');
+let sizeInput = document.querySelector('#size');
+let imageInput = document.querySelector('#image');
+let mainContainer = document.querySelector('#cupcakes-container');
+let URL = 'http://localhost:5000';
 
-let container = document.getElementById('cupcakes-container')
-let URL = 'http://localhost:5000'
+document.addEventListener('DOMContentLoaded', () => {
+    const getData = async (e) => {
+        e.preventDefault()
 
-const getData = async () => {
-    await axios.get(`${URL}/api/cupcakes`).then(response => {
-        for (let cupcake of response.data.cupcakes) {
-            const flavor = cupcake.flavor
-            const image = cupcake.image
-            const size = cupcake.size
-            const rating = cupcake.rating
-            const ckContainer = document.createElement('div')
-            
-            // const flavorContainer = document.createElement('p')
-            // const imageContainer = document.createElement('img')
-            // const sizeContainer = document.createElement('p')
-            // const ratingContainer = document.createElement('div')
+        await axios.get(`${URL}/api/cupcakes`).then(response => {
+            for (let cupcake of response.data.cupcakes) {
+                const flavor = cupcake.flavor;
+                const image = cupcake.image;
+                const size = cupcake.size;
+                const rating = cupcake.rating;
+                const cupcakeDiv = document.createElement('div');
 
-            // flavorContainer.innerHTML = flavor
-            // imageContainer.setAttribute('src', image)
-            // sizeContainer.innerHTML = size
-            // ratingContainer.innerHTML = rating
+                const flavorEl = document.createElement('p');
+                flavorEl.textContent = flavor;
+                const imageEl = document.createElement('img');
+                imageEl.setAttribute('src', image);
+                const sizeEl = document.createElement('p');
+                sizeEl.textContent = size;
+                const ratingEl = document.createElement('span')
+                ratingEl.textContent = rating;
+                cupcakeDiv.append(flavorEl, imageEl, sizeEl, ratingEl);
 
-            // ckContainer.append(flavorContainer)
-            // ckContainer.append(imageContainer)
-            // ckContainer.append(sizeContainer)
-            // ckContainer.append(ratingContainer)
+                mainContainer.appendChild(cupcakeDiv);
+                
+            }
+        })
+    }
 
-            // container.append(ckContainer)
-            
-        }
-    })
-}
+    const postData = async (e) => {
+        e.preventDefault();
 
-const postData = async (e) => {
-    e.preventDefault();
+        let flavor = flavorInput.value;
+        let rating = ratingInput.value;
+        let size = sizeInput.value;
+        let image = imageInput.value;
 
-    let flavor = flavorInput.value
-    let rating = ratingInput.value
-    let size = sizeInput.value
-    let image = imageInput.value
+        await axios.post(`${URL}/api/cupcakes`, {
+            flavor,
+            rating,
+            size,
+            image
+        }).then(response => {
+            alert('CREATED THE CUPCAKE!')
+            console.log(response)
+        })
 
-    await axios.post(`${URL}/api/cupcakes`, {
-        flavor,
-        rating,
-        size,
-        image
-    }).then(() => {
-        alert('CREATED THE CUPCAKE!')
-    })
-}
+        flavorInput.value = '';
+        ratingInput.value = '';
+        sizeInput.value = '';
+        imageInput.value = '';
+    }
 
-getButton.addEventListener('click', getData)
-addButton.addEventListener('click', postData)
+    getButton.addEventListener('click', getData);
+    addButton.addEventListener('click', postData);
+
+});
